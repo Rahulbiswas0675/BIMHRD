681aa22766d13dd83a6af5f84e6512d7e590e831
Rahul Biswas <rahul.b@techpax.in>
2023-12-29 16:54:22 +0530

    placement page done

--- src/PageComponent/CampusAmbassador/CampusAmbassador.jsx
+++ src/PageComponent/CampusAmbassador/CampusAmbassador.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import './CampusAmbassador.scss';
+import google from '../../Assates/CampusingComapnys/google.png';
+import ibm from '../../Assates/CampusingComapnys/ibm.png';
+import deloitte from '../../Assates/CampusingComapnys/deltotl.png';
+import genpact from '../../Assates/CampusingComapnys/genpact.png';
+import xebia from '../../Assates/CampusingComapnys/xebia.png';
+import wipro from '../../Assates/CampusingComapnys/wipro.png';
+import hcl from '../../Assates/CampusingComapnys/hcl.png';
+
 
 function CampusAmbassador() {
   return (
-    <div>CampusAmbassador</div>
+    <div className="capusambassador-component">
+      <div className="capusambassador-container">
+          <img src={google} alt="google" className="icons" />
+          <img src={ibm} alt="ibm" className="icons" />
+          <img src={deloitte} alt="deloitte" className="icons" />
+          <img src={genpact} alt="genpact" className="icons" />
+          <img src={xebia} alt="xebia" className="icons" />
+          <img src={wipro} alt="wipro" className="icons" />
+          <img src={hcl} alt="hcl" className="icons" />
+      </div>
+    </div>
   )
 }
 
--- src/PageComponent/Placements/Placements.jsx
+++ src/PageComponent/Placements/Placements.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import './Placements.scss';
+import { SiSololearn } from "react-icons/si";
+import { GiReceiveMoney } from "react-icons/gi";
+import { BsBank } from "react-icons/bs";
+import { PiStudentBold } from "react-icons/pi";
+import { MdOutlineSupportAgent } from "react-icons/md";
+
 
 function Placements() {
   return (
-    <div>Placements</div>
+    <div className="placement-component">
+      <div className="placement-top-container">
+        <div className="item-box">
+          <SiSololearn className='icon' />
+          <h3 className="title">Admission Process</h3>
+        </div>
+        <div className="item-box">
+          <GiReceiveMoney className='icon' />
+          <h3 className="title">Fee Structure</h3>
+        </div>
+        <div className="item-box">
+          <BsBank className='icon' />
+          <h3 className="title">Scholarships</h3>
+        </div>
+        <div className="item-box">
+          <PiStudentBold className='icon' />
+          <h3 className="title">Lateral Entry & Migration</h3>
+        </div>
+        <div className="item-box">
+          <MdOutlineSupportAgent className='icon' />
+          <h3 className="title">Finance Assistance </h3>
+        </div>
+      </div>
+      <div className="placement-buttom-container">
+        <div className="buttom-left-container">
+          <h3 className="title">Exceptional Placements</h3>
+          <h3 className="tag">BIMHRD</h3>
+          <p className="para">Established in 2004, BIMHRD is the fastest emerging B-School for Human Resources , marketing and other domains of management. Faculty at BIMHRD have vast teaching experience with lots of research papers carried out by them that are accepted nationally and internationally. Faculty members include corporate leaders, industrialists and working professionals from renowned companies.</p>
+          <a href="#" className="readmore-btn">Read More</a>
+        </div>
+        <div className="buttom-right-container">
+          <div className="right-item-box">
+            <h3 className="title">36 LPA</h3>
+            <h5 className="tag">Highest Package</h5>
+          </div>
+          <div className="right-item-box">
+            <h3 className="title">500 +</h3>
+            <h5 className="tag">Recruiters</h5>
+          </div>
+          <div className="right-item-box">
+            <h3 className="title">50k+</h3>
+            <h5 className="tag">Strong Alumni Base</h5>
+          </div>
+          <div className="right-item-box">
+            <h3 className="title">100%</h3>
+            <h5 className="tag">Training and Placement Assistance</h5>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
