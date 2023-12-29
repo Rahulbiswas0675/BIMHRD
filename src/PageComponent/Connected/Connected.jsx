21b93b570c1ce64b252619a025cf606cf6608bcc
Rahul Biswas <rahul.b@techpax.in>
2023-12-30 03:59:22 +0530

    connected  page done

--- src/PageComponent/Connected/Connected.jsx
+++ src/PageComponent/Connected/Connected.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import './Connected.scss';
+import buildImg from '../../Assates/Images/build.png';
+import logo from '../../Assates/Logo/logo4.png';
+import { MdArrowRightAlt } from "react-icons/md";
 
 function Connected() {
   return (
-    <div>Connected</div>
+    <div className="connected-component">
+      <div className="connected-container">
+
+        <div className="connected-container-box">
+          <div className="top-container">
+            <img src={logo} alt="logo" className='logo' />
+            <div className="heading-box">
+              <h1 className="title">Stay Connected, Stay Inspired</h1>
+              <h3 className="tag">Explore the Latest News and Events at BIMHRD</h3>
+            </div>
+          </div>
+
+          <div className="buttom-container">
+
+            <div className="container-item">
+
+              <div className="item-box-left">
+                <div className="box-item-left">
+                  <p className="small-title">Date</p>
+                  <h3 className="small-tag">August 14, 2023</h3>
+                </div>
+                <div className="box-item-right">
+                  <p className="small-title">Event</p>
+                  <h3 className="bold-tag">Industry 5.0 National Conference</h3>
+                </div>
+              </div>
+
+              <div className="divider"></div>
+
+              <div className="item-box-right">
+                <div className="box-item-left">
+                  <p className="small-title">Date</p>
+                  <h3 className="small-tag">August 14, 2023</h3>
+                </div>
+                <div className="box-item-right">
+                  <p className="small-title">Event</p>
+                  <h3 className="bold-tag">Industry 5.0 National Conference</h3>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="buttom-border"></div>
+
+            <div className="container-item">
+
+              <div className="item-box-left">
+                <div className="box-item-left">
+                  <p className="small-title">Date</p>
+                  <h3 className="small-tag">August 14, 2023</h3>
+                </div>
+                <div className="box-item-right">
+                  <p className="small-title">Event</p>
+                  <h3 className="bold-tag">Industry 5.0 National Conference</h3>
+                </div>
+              </div>
+
+              <div className="divider"></div>
+
+              <div className="item-box-right">
+                <div className="box-item-left">
+                  <p className="small-title">Date</p>
+                  <h3 className="small-tag">August 14, 2023</h3>
+                </div>
+                <div className="box-item-right">
+                  <p className="small-title">Event</p>
+                  <h3 className="bold-tag">Industry 5.0 National Conference</h3>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="buttom-border"></div>
+
+          </div>
+        </div>
+      </div>
+      <div className="connected-buttom-container">
+        <img src={buildImg} alt="buildImg" className="image" />
+        <div className="buttom-container">
+          <div className="text-box">
+            <h1 className="title">Explore life at BIMHRD</h1>
+            <button className="button">Let’s Go <MdArrowRightAlt className='icon'/></button>
+          </div>
+          <div className="buttom-shadow"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Connected
+export default Connected;
