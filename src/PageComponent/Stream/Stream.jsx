b5f3262bd602d422db3ae7fca2de3d607095043e
Rahul Biswas <rahul.b@techpax.in>
2023-12-29 05:57:14 +0530

    stream page done

--- src/PageComponent/Stream/Stream.jsx
+++ src/PageComponent/Stream/Stream.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import './Stream.scss';
+import streamImg from '../../Assates/Images/stream.png';
 
 function Stream() {
   return (
-    <div>Stream</div>
+    <div className="stream-components">
+      <img src={streamImg} alt="streamImg" className="streamImg" />
+      <div className="stream-container">
+        <div className="stream-item-box">
+          <h3 className="title">MBA</h3>
+          <h3 className="tag">Genarel</h3>
+          <p className="para">BIMHRD offers a two-year full-time management program MBA (Marketing). As incoming students are mostly freshers or having some years of work experience, the course enable students to grasp the latest in management theory, broadens their perspectives, launches them into new avenues of problem solving and making them competent for the business world.</p>
+        </div>
+        <div className="stream-item-box mid-box">
+          <h3 className="title">MBA</h3>
+          <h3 className="tag">( PM & HRD)</h3>
+          <p className="para">Studying PM & HRD with BIMHRD will help develop your interpersonal skills which are crucial to succeed in any relevant role. Our courses are career-focused which enables you to acquire skills directly applied in your current or future industry role.</p>
+        </div>
+        <div className="stream-item-box">
+          <h3 className="title">MBA</h3>
+          <h3 className="tag">(Marketing Management)</h3>
+          <p className="para">BIMHRD offers a two-year full-time program in MBA (Marketing Management). The program helps the students to equip themselves with the practical knowledge of current industry scenarios along with course curriculum , skills and value.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
